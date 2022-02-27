4d9b269f179ca986d58e8da291b8978e871724b3
Daksh Balyan <dbalyan@daksh-MacBook-Pro.local>
2022-02-27 15:09:41 +0530

    added treemap

---
+++ src/org/personal/dsa/hashing/MainDriver.java
@@ -0,0 +1,7 @@
+package org.personal.dsa.hashing;
+
+public class MainDriver {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/org/personal/dsa/hashing/TreeMap.java
@@ -0,0 +1,13 @@
+package org.personal.dsa.hashing;
+
+public class TreeMap {
+    Object key;
+    Object val;
+    TreeMap left;
+    TreeMap right;
+    public TreeMap(){}
+    public TreeMap(Object key, Object val){
+        this.key = key;
+        this.val = val;
+    }
+}
